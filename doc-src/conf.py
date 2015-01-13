8dcbfa8d0419d8588342e55399731ff3beb4364f
Miguel Pilar <miguel.pilar@gfrmedia.com>
2015-01-13 15:53:49 -0400

    Update copyright year

--- doc-src/conf.py
+++ doc-src/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'mpilar-resume'
-copyright = u'2012, Miguel Pilar'
+copyright = u'2014, Miguel Pilar'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
